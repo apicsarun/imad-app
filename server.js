e0b9eb4eb31df8c2a71535fc746220866797d236
apicsarun <apicsarun@gmail.com>
2017-08-13 16:29:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,19 +95,6 @@ app.get('/:article', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-//app.get('/article-two', function (req, res) {
-//  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-//  var articleName = 'articleTwo';
-//  res.send(createTemplate(articles[articleName]));
-//});
-
-///app.get('/article-three', function (req, res) {
- // res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
- /* var articleName = 'articleThree';
- res.send(createTemplate(articles[articleName]));
-});
-*/
-
 
  app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
