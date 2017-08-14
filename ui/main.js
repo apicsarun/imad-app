a1e80f78dfe3131e5be5989126d0b3d45bf0216d
apicsarun <apicsarun@gmail.com>
2017-08-14 22:45:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,12 +9,12 @@ console.log('Before Click');
 marginLeft = 0;
 
 function moveRight(){
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 
 img.onclick = function(){
-      var interval = setInterval(moveRight,100);
+      var interval = setInterval(moveRight,50);
       
     };
     
